83eba89170ce8b10253115f017dc9d8a0756483d
Abhijit-singh04 <2001.abhijitsingh@gmail.com>
2023-05-20 10:32:28 +0530

    first commit

---
+++ Example/ScalingCarousel/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ScalingCarousel
+//
+//  Created by Abhijit Sing.
+//  
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        return true
+    }
+}
+
